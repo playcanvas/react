6354805d00c12d26fd471158b2a68b9b2c8295e0
Mark Lundin <mlundin@snapchat.com>
2025-01-06 15:30:43 +0000

    Refactor layout component by removing unused Banner import
    - Removed the unused `Banner` import from `layout.tsx`

--- packages/docs/src/app/layout.tsx
+++ packages/docs/src/app/layout.tsx
@@ -1,7 +1,7 @@
 
 /* eslint-env node */
 import { Footer, Layout, Navbar } from 'nextra-theme-docs'
-import { Banner, Head } from 'nextra/components'
+import { Head } from 'nextra/components'
 import { getPageMap } from 'nextra/page-map'
 import './globals.css'
 import { CodeXml } from 'lucide-react'
