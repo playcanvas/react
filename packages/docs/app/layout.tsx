25aff8b1571e04ab0d5ddb8d1ea0576209ba4590
Mark Lundin <mlundin@snapchat.com>
2025-04-30 14:33:20 +0100

    Refactor documentation structure and remove unused components
    - Updated import paths for ReactQueryProvider and other components to ensure consistency.
    - Deleted unused files and components, including various example pages and utility functions, to streamline the codebase.
    - Adjusted TypeScript configuration for improved module resolution.
    - Enhanced overall organization of the documentation structure for better clarity and maintainability.

---
+++ packages/docs/app/layout.tsx
@@ -0,0 +1,78 @@
+
+/* eslint-env node */
+import './globals.css'
+
+import { Footer, Layout, Navbar } from 'nextra-theme-docs'
+import { Banner, Head } from 'nextra/components'
+import { getPageMap } from 'nextra/page-map'
+import { CodeXml } from 'lucide-react'
+import ReactQueryProvider from '@docs-components/ReactQueryProvider'
+
+export const metadata = {
+  metadataBase: new URL('https://playcanvas.com'),
+  title: {
+    template: '%s - PlayCanvas React'
+  },
+  description: 'PlayCanvas React: Full featured 3D for React',
+  applicationName: 'PlayCanvas React',
+  generator: 'Next.js',
+  appleWebApp: {
+    title: 'PlayCanvas React'
+  },
+  other: {
+    'msapplication-TileImage': '/ms-icon-144x144.png',
+    'msapplication-TileColor': '#fff'
+  },
+  twitter: {
+    site: 'https://twitter.com/playcanvas'
+  }
+}
+
+export default async function RootLayout({ children }: { children: React.ReactNode }) {
+  const navbar = (
+    <Navbar
+      
+      logo={
+        <div className="flex items-center gap-2">
+          <div className="rounded-full bg-zinc-800 p-2 inline-block">
+            <CodeXml size={24} strokeWidth={2} />
+          </div>
+          <span className='font-bold'>@playcanvas/react</span>
+          <span style={{ opacity: '60%' }}>- Declarative 3D</span>
+        </div>
+      }
+      // PlayCanvas discord server
+      chatLink="https://discord.gg/EJuENf4hFj"
+
+      // PlayCanvas GitHub
+      projectLink="https://github.com/playcanvas/react"
+    />
+  )
+  return (
+    <html lang="en" dir="ltr" suppressHydrationWarning>
+      <Head faviconGlyph="✦" />
+      <body>
+        <Layout
+          banner={<Banner storageKey="0.3.0-release"><a href="https://github.com/playcanvas/react/releases/tag/v0.3.0" target="_blank" rel="noreferrer">
+            🎉 <b>@playcanvas/react 0.3.0!</b> Now with <b>React 19</b> support. Read more →
+          </a></Banner>}
+          navbar={navbar}
+          footer={<Footer>
+            MIT {new Date().getFullYear()} ©{' '} PlayCanvas.
+          </Footer>}
+          editLink="Edit this page on GitHub"
+          docsRepositoryBase="https://github.com/playcanvas/react/tree/main/packages/docs"
+          sidebar={{ 
+            defaultMenuCollapseLevel: 1,
+            toggleButton: true
+          }}
+          pageMap={await getPageMap()}
+        >
+          <ReactQueryProvider>
+            {children}
+          </ReactQueryProvider>
+        </Layout>
+      </body>
+    </html>
+  )
+}
--- packages/docs/next.config.js
+++ packages/docs/next.config.js
@@ -13,31 +13,16 @@ const withNextra = nextra({
   search: {
     codeblocks: false
   },
-  contentDirBasePath: '/examples',
 })
 
 export default withNextra({
   reactStrictMode: true,
   transpilePackages: ['next-mdx-remote-client', 'playcanvas'],
-  async headers() {
-    return [
-        {
-            // matching all API routes
-            source: "/",
-            headers: [
-                { key: "Access-Control-Allow-Credentials", value: "true" },
-                { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
-                { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
-                { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-            ]
-        }
-    ]
-  },
   redirects: async () => [
     {
-        source: '/examples',
-        destination: '/examples/model-viewer',
-        permanent: true
+      source: '/examples',
+      destination: '/examples/model-viewer',
+      permanent: true
     },
     {
       source: '/',
