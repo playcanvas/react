26f0009917ca0e270f2980de25ca7da9951c9a49
Mark Lundin <mlundin@snapchat.com>
2025-05-30 13:50:39 +0100

    better V0 layout

--- packages/docs/registry/splat-viewer.tsx
+++ packages/docs/registry/splat-viewer.tsx
@@ -4,18 +4,20 @@ const splatUrl = "https://d28zzqy0iyovbz.cloudfront.net/30b943d5/scene.compresse
 
 export function SplatViewer() {
     return (
-      <Viewer.Splat src={splatUrl} className="rounded-lg shadow-xl cursor-grab active:cursor-grabbing" >
-        <Viewer.Controls >
-          <div className="flex gap-1 pointer-events-auto flex-grow">
-              <Viewer.FullScreenButton />
-              <Viewer.DownloadButton />
-          </div>
-          <div className="flex gap-1 pointer-events-auto">
-            <Viewer.CameraModeToggle />
-            <Viewer.HelpButton />
-            <Viewer.MenuButton />
-          </div>
-        </Viewer.Controls>
-    </Viewer.Splat>
+      <div className="flex flex-col items-center justify-center min-h-screen gap-4"> 
+        <Viewer.Splat src={splatUrl} className="rounded-lg shadow-xl cursor-grab active:cursor-grabbing" >
+          <Viewer.Controls >
+            <div className="flex gap-1 pointer-events-auto flex-grow">
+                <Viewer.FullScreenButton />
+                <Viewer.DownloadButton />
+            </div>
+            <div className="flex gap-1 pointer-events-auto">
+              <Viewer.CameraModeToggle />
+              <Viewer.HelpButton />
+              <Viewer.MenuButton />
+            </div>
+            </Viewer.Controls>
+        </Viewer.Splat>
+      </div>
     )
   }
