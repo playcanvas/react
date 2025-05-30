fe43d139e82e665ba842ad7a2d2c5b30b505883a
Mark Lundin <mlundin@snapchat.com>
2025-05-30 12:20:53 +0100

    remove testing code

--- packages/blocks/src/splat-viewer/smart-camera.tsx
+++ packages/blocks/src/splat-viewer/smart-camera.tsx
@@ -87,17 +87,17 @@ export function SmartCamera({
     }
 
     const initialPose = computeStartingPose(gsplat, fov);
-    // initialPose.position = [
-    //     -8.103486061096191,
-    //     1.483533501625061,
-    //     5.499182224273682
-    // ]
-    // initialPose.target = [
-    //   -8.103407561083364,
-    //   1.483563169807258,
-    //   5.498786613731963
-    // ]
-    // console.log('initialPose', initialPose);
+    initialPose.position = [
+        -8.103486061096191,
+        1.483533501625061,
+        5.499182224273682
+    ]
+    initialPose.target = [
+      -8.103407561083364,
+      1.483563169807258,
+      5.498786613731963
+    ]
+    console.log('initialPose', initialPose);
     //   const transitionStartRef = useRef<{ pos: Vec3; rot: Quat } | null>(null);
     
     setPose(initialPose);
