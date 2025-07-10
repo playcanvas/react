fb50f2a7ba5a7d84c474d64b5005d535d1a56760
Mark Lundin <mlundin@snapchat.com>
2025-07-10 11:30:45 +0100

    migrates to playcanvas Grid

--- packages/docs/components/Grid.tsx
+++ packages/docs/components/Grid.tsx
@@ -1,9 +1,13 @@
+import { Entity } from "@playcanvas/react";
 import { Script } from "@playcanvas/react/components";
-import { Grid as GridScript } from "@playcanvas/react/scripts";
+import { Grid as GridScript } from "playcanvas/scripts/esm/grid.mjs";
 import { FC } from "react";
 
 const Grid: FC = ({ ...props }) => {
-    return <Script script={GridScript} {...props} />
+
+    return <Entity scale={[1000, 1000, 1000]}>
+        <Script script={GridScript} {...props} />
+    </Entity>
 }
 
 export default Grid;
