9a84d4ef82d1f9040e9d5d2ee3bed69283a9f4ce
Mark Lundin <mlundin@snapchat.com>
2025-08-11 13:03:05 +0100

    removing pointer event

--- packages/lib/src/utils/picker.tsx
+++ packages/lib/src/utils/picker.tsx
@@ -154,7 +154,6 @@ export const usePicker = (app: AppBase | null, el: HTMLElement | null, pointerEv
     // Construct a generic handler for pointer events
     const onInteractionEvent = useCallback(async (e: MouseEvent)  => {
         if (!picker || !app || !canvasRectRef.current) return;
-        if (pointerEvents.size === 0) return;
 
         const entity = await getEntityAtPointerEvent(app, picker, canvasRectRef.current, e);
 
