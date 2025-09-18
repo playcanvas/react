17f0d5ef24dc42fe4876d25611496da7adba3119
Mark Lundin <mlundin@snapchat.com>
2025-09-18 16:39:11 +0100

    feat(docs): update HomePageExample to use new Environment component with showSkybox prop

---
+++ .changeset/stupid-peas-hunt.md
@@ -0,0 +1,5 @@
+---
+"@playcanvas/react": minor
+---
+
+Adds a new `<Environment showSkybox={false}/>` props which can hides the skybox layer.
--- packages/docs/templates/HomePageExample.jsx
+++ packages/docs/templates/HomePageExample.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Container, Entity } from '@playcanvas/react';
-import { Camera, EnvAtlas } from '@playcanvas/react/components';
+import { Camera, Environment } from '@playcanvas/react/components';
 import { OrbitControls } from '@playcanvas/react/scripts';
 
 import { useEnvAtlas, useModel } from '@components/hooks/use-asset';
@@ -12,15 +12,15 @@ import ShadowCatcher from '@components/ShadowCatcher';
 
 const Example = () => {
 
-    const { data: envMap } = useEnvAtlas('/environment-map.png');
+    const { data: envAtlas } = useEnvAtlas('/environment-map.png');
     const { data: model } = useModel('/lamborghini_vision_gt.glb');
 
     return (<Entity>
-        <EnvAtlas asset={envMap} skyboxIntensity={1.2} showSkybox={false} />
+        { envAtlas && <Environment envAtlas={envAtlas} showSkybox={false}/> }
         <Grid />
         <ShadowCatcher width={5} depth={5} />
         <Entity name='camera' position={[4, 1, 4]}>
-            <Camera clearColor='#090707' fov={28} />
+            <Camera clearColor='#090707' fov={28} renderSceneColorMap={true}/>
             <OrbitControls inertiaFactor={0.07} distanceMin={6} distanceMax={10} pitchAngleMin={1} pitchAngleMax={90}/>
             <StaticPostEffects />
             <AutoRotate />
