4968d3ade0d9ba2e2c28bcd76ab26fe6b189b2d7
Mark Lundin <mlundin@snapchat.com>
2025-09-22 12:19:18 +0100

    refactor: update Grid component usage in documentation and examples to reflect new entity structure

--- packages/docs/components/Grid.tsx
+++ packages/docs/components/Grid.tsx
@@ -6,7 +6,7 @@ import { Grid as GridScript } from "playcanvas/scripts/esm/grid.mjs";
 import { FC } from "react";
 
 const Grid: FC = ({ ...props }) => {
-    return <Entity scale={[1000, 1000, 1000]}>
+    return <Entity scale={[1000, 1, 1000]}>
         <Script script={GridScript} {...props} />
     </Entity>
 }
