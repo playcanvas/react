0eec15e9f3d4dc65884c8ce036f7b85b139bb579
Mark Lundin <mlundin@snapchat.com>
2025-09-22 10:36:11 +0100

    refactor: remove deprecated Grid script and update exports in index.ts

--- packages/lib/src/scripts/index.ts
+++ packages/lib/src/scripts/index.ts
@@ -1,6 +1,5 @@
 "use client";
 
-export { Grid } from './infinite-grid/infinite-grid.js';
 export { OrbitControls } from './orbit-controls/index.tsx';
 export { ShadowCatcher } from './shadow-catcher/index.js';
 export { AutoRotator } from './auto-rotator/auto-rotator.tsx';
