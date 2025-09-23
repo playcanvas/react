f1580ca9a48dbb39f93eea4d77ab7215af19e9b3
Mark Lundin <mlundin@snapchat.com>
2025-09-23 12:25:09 +0100

    Docs patch

--- packages/docs/client-mdx-components.js
+++ packages/docs/client-mdx-components.js
@@ -1,5 +1,6 @@
 import { Application, Entity } from '@playcanvas/react'
-import { OrbitControls } from '@playcanvas/react/scripts'
+// import { OrbitControls } from '@playcanvas/react/scripts'
+import { CameraControls } from 'playcanvas/scripts/esm/camera-controls.mjs'
 import { Align, Light, Anim, Camera, Collision, GSplat, Script, Render, RigidBody, Environment } from '@playcanvas/react/components'
 
 import ReactQueryProvider from '@docs-components/ReactQueryProvider'
@@ -13,8 +14,12 @@ import { MotionEntity, MotionLight } from '@components/MotionEntity'
 import { Glb } from '@components/Glb'
 import { useEnvAtlas } from '@playcanvas/react/hooks'
 
-import { version } from 'playcanvas';
+import { Vec2, version } from 'playcanvas';
 const PcVersion = () => <span>{version}</span>;
+
+export const OrbitControls = ({ zoomRange = [1, 10], pitchRange = [-90, -5], ...props} ) => {
+    return <Script script={CameraControls} enableFly={false} zoomRange={new Vec2(zoomRange[0], zoomRange[1])} pitchRange={new Vec2(pitchRange[0], pitchRange[1])} {...props} />
+};
 
 export const defaultComponents = {
     PcVersion,
--- packages/docs/templates/HomePageExample.jsx
+++ packages/docs/templates/HomePageExample.jsx
@@ -1,8 +1,9 @@
 "use client";
 
 import { Container, Entity } from '@playcanvas/react';
-import { Camera, Environment } from '@playcanvas/react/components';
-import { OrbitControls } from '@playcanvas/react/scripts';
+import { Camera, Environment, Script } from '@playcanvas/react/components';
+import { CameraControls } from 'playcanvas/scripts/esm/camera-controls.mjs';
+import { Vec2 } from 'playcanvas';
 
 import { useEnvAtlas, useModel } from '@components/hooks/use-asset';
 import Grid from '@components/Grid';
@@ -10,18 +11,23 @@ import AutoRotate from '@components/AutoRotate';
 import { StaticPostEffects } from '@components/PostEffects';
 import ShadowCatcher from '@components/ShadowCatcher';
 
+const OrbitControls = ({ zoomRange = [1, 10], pitchRange = [-90, -5], ...props} ) => {
+    return <Script script={CameraControls} enableFly={false} zoomRange={new Vec2(zoomRange[0], zoomRange[1])} pitchRange={new Vec2(pitchRange[0], pitchRange[1])} {...props} />
+};
+
+
 const Example = () => {
 
     const { data: envAtlas } = useEnvAtlas('/environment-map.png');
     const { data: model } = useModel('/lamborghini_vision_gt.glb');
 
     return (<Entity>
-        { envAtlas && <Environment envAtlas={envAtlas} showSkybox={false}/> }
+        { envAtlas && <Environment envAtlas={envAtlas} showSkybox={false} skyboxIntensity={2}/> }
         <Grid />
         <ShadowCatcher width={5} depth={5} />
         <Entity name='camera' position={[4, 1, 4]}>
             <Camera clearColor='#090707' fov={28} renderSceneColorMap={true} />
-            <OrbitControls inertiaFactor={0.07} distanceMin={6} distanceMax={10} pitchAngleMin={1} pitchAngleMax={90}/>
+            <OrbitControls inertiaFactor={0.07} zoomRange={[6, 10]} pitchRange={[-90, -5]}/>
             <StaticPostEffects />
             <AutoRotate />
         </Entity>
