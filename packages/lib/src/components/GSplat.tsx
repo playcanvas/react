e564a0e5e949a9f670256369f1b421ef3e6d5d7f
abstrakt <68276899+abstrakt8@users.noreply.github.com>
2025-12-29 14:58:24 +0000

    fix: Apply GSplat `unified` property while it is disabled (#299)

--- packages/lib/src/components/GSplat.tsx
+++ packages/lib/src/components/GSplat.tsx
@@ -36,5 +36,29 @@ componentDefinition.schema = {
         validate: (val: unknown) => val instanceof Asset,
         errorMsg: (val: unknown) => `Invalid value for prop "asset": "${JSON.stringify(val)}". Expected an Asset.`,
         default: null
+    },
+    // `unified` is a special property that can not be modified while the component is enabled
+    unified: {
+        validate: (val: unknown) => typeof val === 'boolean',
+        errorMsg: (val: unknown) => `Invalid value for prop "unified": "${JSON.stringify(val)}". Expected a boolean.`,
+        default: false,
+        apply: (instance: GSplatComponent, props: Record<string, unknown>, key: string) => {
+            const value = props[key] as boolean;
+
+            if (instance.unified === value) {
+                return;
+            }
+
+            // If not enabled, just set directly
+            if (!instance.enabled) {
+                instance.unified = value;
+                return;
+            }
+
+            // Temporarily disable component, set value, re-enable
+            instance.enabled = false;
+            instance.unified = value;
+            instance.enabled = true;
+        }
     }
 }
